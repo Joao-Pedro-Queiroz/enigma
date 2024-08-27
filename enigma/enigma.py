bc090119dc90f0503ffc1f17cf44d60a95f574e7
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-08-27 12:31:07 -0300

    função de permutação funcional

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -1,5 +1,15 @@
-import numpy
+import numpy as np
+from typing import Tuple
 
 
 def one_hot_encoding(mensagem):
     
+
+
+def gerar_matrizes_de_permutacao(N : int) -> Tuple[np.ndarray, np.ndarray]:
+    identidade = np.eye(N)
+
+    P = np.random.permutation(identidade)
+    R = np.random.permutation(identidade)
+
+    return (P, R)
