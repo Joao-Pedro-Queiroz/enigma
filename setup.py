b030e50fd893dae553ba057e84ee96c35546c2cf
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-08-27 10:52:53 -0300

    definindo base do projeto

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="enigma",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'enigma=enigma.enigma:main',
+        ],
+    },
+    author="João Pedro Queiroz Viana, Pedro Fardin",
+    author_email="joaopqv@al.insper.edu.br, ",
+    description="Uma biblioteca de criptografia.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Joao-Pedro-Queiroz/enigma",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
