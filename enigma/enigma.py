605cf6c91d971167d22542b36a26c1c71e389a5b
pedrofardin <pedrorf1@al.insper.edu.br>
2024-08-27 12:03:40 -0300

    feat/one_hot_coding

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -1,5 +1,14 @@
-import numpy
+import numpy as np
 
 
 def one_hot_encoding(mensagem):
+    dicionario = {}
+    for i, letra in enumerate(mensagem):
+        if letra not in dicionario:
+            dicionario[letra] = len(dicionario)
     
+    matriz = np.zeros((len(mensagem), len(dicionario)))
+    for i, letra in enumerate(mensagem):
+        matriz[i, dicionario[letra]] = 1
+    
+    return matriz
