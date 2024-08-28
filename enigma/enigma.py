a4386db2d260a089eef50f30a514f11defe7a5ad
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-08-28 07:42:22 -0300

    adicionando espaço para fazer as últimas funções

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -24,4 +24,14 @@ def gerar_matrizes_de_permutacao(N : int) -> Tuple[np.ndarray, np.ndarray]:
 
     return (P, R)
 
-print(one_hot_encoding("olal"))
+
+def encriptar_enigma(mensagem : str,
+              P : np.ndarray,
+              Q : np.ndarray) -> str:
+    raise NotImplementedError
+
+
+def decriptar_enigma(mensagem_encriptada : str,
+              P : np.ndarray,
+              Q : np.ndarray) -> str:
+    raise NotImplementedError
