ecad39f831d25f9f119f32a6fd94e534504dd085
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-08-29 17:37:21 -0300

    Funções prontas

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -27,16 +27,58 @@ def gerar_matrizes_de_permutacao(N : int) -> Tuple[np.ndarray, np.ndarray]:
     identidade = np.eye(N)
 
     P = np.random.permutation(identidade)
-    R = np.random.permutation(identidade)
+    Q = np.random.permutation(identidade)
 
-    return (P, R)
+    return (P, Q)
 
 
 def encriptar_enigma(mensagem : str, P : np.ndarray, Q : np.ndarray) -> str:
-    raise NotImplementedError
+    dicionario = fazer_dict()
+    matriz_mensagem = one_hot_encoding(mensagem)
+    matriz_permutacao = P @ Q
+    matriz_permutada = matriz_permutacao @ matriz_mensagem
+    mensagem_codificada = ''
+    contador = 0
+
+    while len(mensagem_codificada) < len(mensagem):
+        for l in range(len(matriz_permutada)):
+            if matriz_permutada[l][contador] == 1:
+                for caractere in dicionario.keys():
+                    if dicionario[caractere] == l:
+                        mensagem_codificada += caractere
+                        contador += 1
+                        break
+        
+                break
+
+    return mensagem_codificada
+
 
 
 def decriptar_enigma(mensagem_encriptada : str, P : np.ndarray, Q : np.ndarray) -> str:
-    raise NotImplementedError
+    dicionario = fazer_dict()
+    matriz_mensagem_codificada = one_hot_encoding(mensagem_encriptada)
+    matriz_permutacao = P @ Q
+    matriz_permutada = np.linalg.inv(matriz_permutacao) @ matriz_mensagem_codificada
+    mensagem_descodificada = ''
+    contador = 0
 
-print(one_hot_encoding("Joao"))
+    while len(mensagem_descodificada) < len(mensagem_encriptada):
+        for l in range(len(matriz_permutada)):
+            if matriz_permutada[l][contador] == 1:
+                for caractere in dicionario.keys():
+                    if dicionario[caractere] == l:
+                        mensagem_descodificada += caractere
+                        contador += 1
+                        break
+        
+                break
+
+    return mensagem_descodificada
+
+
+P, Q = gerar_matrizes_de_permutacao(len(fazer_dict()))
+mensagem_codificada = encriptar_enigma("Chocolate", P, Q)
+mensagem_descodificada = decriptar_enigma(mensagem_codificada, P, Q)
+print(mensagem_codificada)
+print(mensagem_descodificada)
